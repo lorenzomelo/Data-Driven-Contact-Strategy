7301e78eb769cefe2f3d21596fa43e0783fbbb3e
FabianaCaccavale <92259415+FabianaCaccavale@users.noreply.github.com>
2022-03-10 15:56:25 +0100

    clean script

--- ELIGIBILITY_DATA_DRIVEN_CONTACT_STRATEGY.py
+++ ELIGIBILITY_DATA_DRIVEN_CONTACT_STRATEGY.py
@@ -1,102 +1,105 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-dataset = pd.read_csv("DLT_AI_and_DATA_CUSTOMER_BASE_EN_PROVA.csv", sep=";")
+dataset = pd.read_csv("DLT_AI_and_DATA_CUSTOMER_BASE_EN.csv", sep=";", encoding = 'unicode_escape')
 dataset = pd.DataFrame(dataset)
 
-set(dataset["COMMODITY"])
-set(dataset["SOLUTIONS"])
-set(dataset["CONSENSUS_PRIVACY"])
+###### EXPLORATORY DATA ANALYSIS #######
 
-#GENERAL CLEANING
+#### SOLUTIONS ####
 '''
-togliamo i "nan" e "NO" dal dal dataset["CONSENSUS_PRIVACY"]
-togliere risk of being churned or outbound
-CLC_STATUS = clc --> customer life cycle
+#COMMODITY
+crosstab_commodity_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["COMMODITY"], normalize='index')
+crosstab_commodity_solutions.plot.bar(figsize=(6, 4),
+                               rot=0).set(ylabel="Count", xlabel = "Solution")
+plt.title('Solutions by commodity type', fontweight="bold", fontsize =10)
+plt.show()
+#PRIVACY
+crosstab_privacy_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["CONSENSUS_PRIVACY"], normalize='index')
+crosstab_privacy_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('red', 'green')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Privacy Consensus', fontweight="bold", fontsize =10)
+plt.show()
+#CUSTOMER SENIORITY
+crosstab_custsen_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["CUSTOMER_SENIORITY"], normalize='index')
+crosstab_custsen_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('hotpink', 'deeppink', 'mediumvioletred')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Customer Seniority', fontweight="bold", fontsize =10)
+plt.show()
+#GENDER
+crosstab_genre_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["GENRE"], normalize='index')
+crosstab_genre_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('lightcoral', 'gold')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Genre', fontweight="bold", fontsize =10)
+plt.show()
+
+#LOYALITY PROGRAM
+crosstab_loyalityprog_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["LOYALTY_PROGRAM"], normalize='index')
+crosstab_loyalityprog_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('darkorchid', 'royalblue')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Loyalty Program', fontweight="bold", fontsize =10)
+plt.show()
+
+#AREA
+crosstab_area_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["AREA"], normalize='index')
+crosstab_area_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('limegreen', 'lightcoral', 'deepskyblue', 'gold')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Area of Italy', fontweight="bold", fontsize =10)
+plt.show()
+
+#FLAG_BAD_CUSTOMER
+crosstab_flag_solutions = pd.crosstab(index=dataset4["SOLUTIONS"],
+                                    columns=dataset4["FLAG_BAD_CUSTOMER"], normalize='index')
+crosstab_flag_solutions.plot.bar(figsize=(6, 4),
+                               rot=0, color=('yellowgreen', 'crimson')).set(ylabel="Percentage", xlabel = "Solution")
+plt.title('Solutions by Bad Customer Flag', fontweight="bold", fontsize =10)
+plt.show()
+
+#AVG CONSUMPTION GAS and AVG CONSUMPTION POWER
+bp = sns.boxplot(data = dataset, x = "SOLUTIONS", y= "AVG_CONSUMPTION_GAS_M3", hue="SOLUTIONS")  # RUN PLOT
+bp.set_ylim([0, 200000])
+plt.show()
+
+#INBOUND CONTACTS LAST YEAR
+hp = sns.histplot(data=dataset, x="INBOUND_CONTACTS_LAST_YEAR")
+hp.set_xlim([0, 20])
+plt.show()
+#N MISSED PAYMENTS
+
+#N CAMPAIGN SENT
+
+#N DEM, N SMS, N TLS SOLUTION
+
+#BEHAVIOUR SCORE
+
+#CLC STATUS
+
+#ACQUISITION CHANNEL
+
+
 '''
-dataset["CONSENSUS_PRIVACY"].isnull().sum()
-dataset2 = dataset[dataset["CONSENSUS_PRIVACY"].notnull()]
-dataset2["CONSENSUS_PRIVACY"].isnull().sum()
 
-consensus_privacy_yes = dataset2["CONSENSUS_PRIVACY"].isin(["YES"])
-dataset3 = dataset2[consensus_privacy_yes]
-set(dataset3["CONSENSUS_PRIVACY"])
-len(dataset3)
-
-dataset2["CONSENSUS_PRIVACY"].value_counts()
-dataset3["SOLUTIONS"].value_counts()
-set(dataset3["CLC_STATUS"])
-dataset3["CLC_STATUS"].value_counts()
-
-
-
-### DIVIDING DATASET ACCORDING TO THE ELIGIBILITY
-pd.options.mode.chained_assignment = None
-timefmt = "%d/%m/%Y"
-dataset3['DATE_LAST_CAMPAIGN'] = pd.to_datetime(dataset3['DATE_LAST_CAMPAIGN'], format = timefmt)
-
-datatypes = dataset3.dtypes
-
-dataset3['REFERENCE_DATE'] = "22/03/2022"
-dataset3['REFERENCE_DATE'] = pd.to_datetime(dataset3['REFERENCE_DATE'], format = timefmt)
-
-dataset3['N_months'] = ((dataset3.DATE_LAST_CAMPAIGN - dataset3.REFERENCE_DATE)/np.timedelta64(1, 'M'))
-dataset3['N_months'] = dataset3['N_months'].astype(int).abs()
-
-cross_selling_tls = dataset3[dataset3.N_months >= 6]
-cross_selling_dem = dataset3[dataset3.N_months >= 2]
-cross_selling_sms = dataset3[dataset3.N_months >= 2]
-solution_tls = dataset3[dataset3.N_months >= 12]
-solution_dem = dataset3[dataset3.N_months >= 6]
-solution_sms = dataset3[dataset3.N_months >= 6]
+#### COMMODITY ####
 
 
 
 
-#DROPS RICK CHURNS AND LEAVING
+
+##### Deleting risk churn and leaving
 dataset4 = dataset.drop(dataset[dataset["CLC_STATUS"] == "4-Risk churn"].index)
 dataset4 = dataset4.drop(dataset4[dataset4["CLC_STATUS"] == "5-Leaving"].index)
-dataset4["CLC_STATUS"].value_counts()
-len(dataset4)
 
 
 
-#DATASET DIVISION
-'''
-DATASET SOLUTION
-drop the people with already a solution --> "SOLUTIONS" == 1
-'''
-
-solution_dataset = dataset4.drop(dataset4[dataset4["SOLUTIONS"] == 1].index)
-set(solution_dataset["SOLUTIONS"])
-len(solution_dataset)
-
-#solution_dataset.to_csv("SOLUTION_DATASET.csv")
-
-#DATASET CROSS_SELLING                                 
-cross_selling_dataset = dataset4.drop(dataset4[dataset4["COMMODITY"] == "DUAL"].index)
-set(cross_selling_dataset["COMMODITY"])
-len(cross_selling_dataset)
-#cross_selling_dataset.to_csv("CROSS_SELLING_DATASET.csv")
-
-                                 
-#### RISK CHURN CORRELATIONS
-#drop the variables not useful for the correlation matrix
-corr_dataset = dataset3.drop(['GENRE', 'ID', 'DATE_LAST_VISIT_DESK','DATE_LAST_REQUEST_CC','FIRST_ACTIVATION_DATE','DATE_LAST_CAMPAIGN','SUPPLY_START_DATE','YEAR_BIRTH','ZONE','AREA'], axis = 1)
-#get dummies of categorical variables
-df_dummies = pd.get_dummies(corr_dataset, columns=['CONSENSUS_PRIVACY','COMMODITY','CUSTOMER_SENIORITY','PHONE_VALIDATED','BEHAVIOUR_SCORE','CLC_STATUS','ACQUISITION_CHANNEL','LAST_GAS_PRODUCT','LAST_POWER_PRODUCT', 'LAST_CAMPAIGN_TIPOLOGY'])
-#drop the other values of the variable CLC status
-riskchurn_corr_df = df_dummies.drop(['CLC_STATUS_3-Customer Loyalty','CLC_STATUS_1-New','CLC_STATUS_2-Customer','CLC_STATUS_5-Leaving'],axis=1)
-#correlation matrix
-corrmatr = riskchurn_corr_df.corr()
-#sort by correlation index to see most correlated variables
-riskchurn_corr = corrmatr['CLC_STATUS_4-Risk churn']
-riskchurn_corr_sorted = riskchurn_corr.sort_values(ascending=False)
-
-                                 
-                                 
-                                 
-                                 
+##### OUTLIER DETECTION ######
 exp_dataset4 = dataset4.filter(['LAST_MONTH_DESK_VISITS', 'LAST_3MONTHS_DESK_VISITS',
        'LAST_YEAR_DESK_VISITS', 'LAST_MONTH_CC_REQUESTS',
        'LAST_3MONTHS_CC_REQUESTS', 'LAST_YEAR_CC_REQUESTS','N_GAS_POINTS', 'N_POWER_POINTS', 'N_DISUSED_GAS_POINTS',
@@ -132,13 +135,22 @@ dataset4 = pd.concat([dataset4.reset_index(drop=True),exp_dataset4.reset_index(d
 descr2 = dataset4.describe().T
 
 
-### Analizziamo cosa caratterizza i "DUAL" e i "SOLUTIONS_1" guardando le medie 
+
+#### dataset for dummies
+df_for_dummies = dataset4.drop(['ID', 'CONSENSUS_PRIVACY', 'DATE_LAST_VISIT_DESK','DATE_LAST_REQUEST_CC',
+                          'FIRST_ACTIVATION_DATE', 'DATE_LAST_CAMPAIGN', 'SUPPLY_START_DATE', 'EMAIL_VALIDATED',
+                          'PHONE_VALIDATED', 'YEAR_BIRTH', "AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH"], axis = 1)
+dummy_df = pd.get_dummies(df_for_dummies)
+
+
+
+
+##### MEAN ANALYSIS #####
+
+### Analizziamo cosa caratterizza i "DUAL" e i "SOLUTIONS_1" guardando le medie
 
 ## Alcune variabili che torneranno utili più tardi.
-# Facciamo una distinzione tra AVG_CONSUPTION e le altre variabili per via della "scala" dei valori 
-dataset5 = dataset4.drop(['GENRE', 'ID', 'CONSENSUS_PRIVACY', 'DATE_LAST_VISIT_DESK','DATE_LAST_REQUEST_CC', 'ZONE', 'AREA', 'FIRST_ACTIVATION_DATE', 'DATE_LAST_CAMPAIGN', 'SUPPLY_START_DATE', 'EMAIL_VALIDATED', 'PHONE_VALIDATED', 'YEAR_BIRTH', "AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH"], axis = 1)
-dummy_df = pd.get_dummies(dataset5)
-
+# Facciamo una distinzione tra AVG_CONSUPTION e le altre variabili per via della "scala" dei valori
 avg_df = dataset4.filter(["AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH", "SOLUTIONS"], axis=1)
 dummy_avg_df = pd.get_dummies(avg_df)
 
@@ -185,15 +197,3 @@ avg_df_mean_2 = avg_df_mean_2.T
 avg_df_mean_2["variables"] = columns2
 avg_df_mean_2["difference"] = abs(avg_df_mean_2[0] - avg_df_mean_2[1])
 avg_df_mean_2.plot(x="variables", y=[0, 1], kind="barh", color = ["green", "red"])
-
-
-#Creating datasets for the logistic regression and the random forest
-rf1 = dummy_df.filter(difference_dataset["variables"].tolist(), axis=1)
-rf2 = dummy_df.filter(difference_dataset_2["variables"].tolist(), axis=1)
-rf1['SOLUTIONS'] = dummy_df['SOLUTIONS']
-rf2['COMMODITY_DUAL'] = dummy_df['COMMODITY_DUAL']
-
-rf1.to_csv('dataset_log_rf1.csv')
-rf2.to_csv('dataset_log_rf2.csv')
-
-
