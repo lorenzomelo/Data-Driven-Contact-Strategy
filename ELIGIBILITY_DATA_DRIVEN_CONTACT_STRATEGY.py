a5251b6179f2b752060523bd496280d354e2622e
lorenzomelo <93031165+lorenzomelo@users.noreply.github.com>
2022-03-25 17:02:47 +0100

    Update ELIGIBILITY_DATA_DRIVEN_CONTACT_STRATEGY.py

--- ELIGIBILITY_DATA_DRIVEN_CONTACT_STRATEGY.py
+++ ELIGIBILITY_DATA_DRIVEN_CONTACT_STRATEGY.py
@@ -135,7 +135,7 @@ dataset4 = pd.concat([dataset4.reset_index(drop=True),exp_dataset4.reset_index(d
 descr2 = dataset4.describe().T
 
 
-
+#### dataset for dummies
 df_for_dummies = dataset4.drop(['ID', 'CONSENSUS_PRIVACY', 'DATE_LAST_VISIT_DESK','DATE_LAST_REQUEST_CC',
                           'FIRST_ACTIVATION_DATE', 'DATE_LAST_CAMPAIGN', 'SUPPLY_START_DATE', 'EMAIL_VALIDATED',
                           'PHONE_VALIDATED', 'YEAR_BIRTH', 
@@ -145,19 +145,20 @@ df_for_dummies = dataset4.drop(['ID', 'CONSENSUS_PRIVACY', 'DATE_LAST_VISIT_DESK
                           'LAST_YEAR_CC_REQUESTS', 'INBOUND_CONTACTS_LAST_MONTH', 'INBOUND_CONTACTS_LAST_2MONTHS',
                           'INBOUND_CONTACTS_LAST_YEAR','N_CAMPAIGN_SENT', 'N_CAMPAIGN_CLICKED', 'N_CAMPAIGN_OPENED',
                           'N_DEM_CARING', 'N_SMS_CARING', 'N_TLS_CARING', 'N_DEM_RENEWAL',
-                          'N_SMS_RENEWAL', 'N_TLS_RENEWAL', 'N_DEM_CROSS_SELLING',
-                          'N_SMS_CROSS_SELLING', 'N_TLS_CROSS_SELLING', 'N_DEM_SOLUTION',
-                          'N_SMS_SOLUTION', 'N_TLS_SOLUTION'], axis = 1)
+                          'N_SMS_RENEWAL', 'N_TLS_RENEWAL'], axis = 1)
+
+
 dummy_df = pd.get_dummies(df_for_dummies)
 
 
-# UNDERSAMPLING
+# UNDERSAMPLING 
 class_2,class_1 = dummy_df.SOLUTIONS.value_counts()
 c2 = dummy_df[dummy_df['SOLUTIONS'] == 0]
 c1 = dummy_df[dummy_df['SOLUTIONS'] == 1]
 df_2 = c2.sample(class_1)
 under_sol = pd.concat([df_2,c1],axis=0)
 under_sol.SOLUTIONS.value_counts()
+under_sol.to_csv("Logistic_solution.csv")
 
 class_2,class_1 = dummy_df.COMMODITY_DUAL.value_counts()
 c2 = dummy_df[dummy_df['COMMODITY_DUAL'] == 0]
@@ -165,6 +166,7 @@ c1 = dummy_df[dummy_df['COMMODITY_DUAL'] == 1]
 df_2 = c2.sample(class_1)
 under_dual = pd.concat([df_2,c1],axis=0)
 under_dual.COMMODITY_DUAL.value_counts()
+under_dual.to_csv("Logistic_dual.csv")
 
 
 ##### MEAN ANALYSIS #####
@@ -182,7 +184,9 @@ dummy_avg_df_2 = dummy_avg_df_2.drop(["COMMODITY_GAS", "COMMODITY_POWER"], axis=
 
 
 ## SOLUTIONS
-df_mean = dummy_df.drop(["AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH"], axis = 1)
+df_mean = dummy_df.drop(["AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH", 'N_DEM_CROSS_SELLING',
+                          'N_SMS_CROSS_SELLING', 'N_TLS_CROSS_SELLING', 'N_DEM_SOLUTION',
+                          'N_SMS_SOLUTION', 'N_TLS_SOLUTION'], axis = 1)
 df_mean = df_mean.groupby('SOLUTIONS').mean()
 df_mean.reset_index(drop=True, inplace=True)
 columns = list(df_mean.columns)
@@ -203,7 +207,9 @@ avg_df_mean.plot(x="variables", y=[0, 1], kind="barh")
 
 
 ## DUAL
-df_mean_2 = dummy_df.drop(["AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH"], axis = 1)
+df_mean_2 = dummy_df.drop(["AVG_CONSUMPTION_GAS_M3", "AVG_CONSUMPTION_POWER_KWH", 'N_DEM_CROSS_SELLING',
+                          'N_SMS_CROSS_SELLING', 'N_TLS_CROSS_SELLING', 'N_DEM_SOLUTION',
+                          'N_SMS_SOLUTION', 'N_TLS_SOLUTION'], axis = 1)
 df_mean_2 = df_mean_2.groupby('COMMODITY_DUAL').mean()
 df_mean_2.reset_index(drop=True, inplace=True)
 columns = list(df_mean_2.columns)
@@ -221,3 +227,9 @@ avg_df_mean_2 = avg_df_mean_2.T
 avg_df_mean_2["variables"] = columns2
 avg_df_mean_2["difference"] = abs(avg_df_mean_2[0] - avg_df_mean_2[1])
 avg_df_mean_2.plot(x="variables", y=[0, 1], kind="barh", color = ["green", "red"])
+
+
+
+
+
+
